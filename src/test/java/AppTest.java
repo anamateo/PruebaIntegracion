e90ce81f0e0c432b3f604bd72fe8e19c173becd6
Ana Mateo <anamateo@tuenti.com>
2014-11-04 16:52:33 +0100

    fail test

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,6 +1,6 @@
 import org.junit.Test;
 
-import static org.junit.Assert.assertTrue;
+import static org.junit.Assert.assertFalse;
 
 /**
  * Created by anamateo on 04/11/14.
@@ -10,7 +10,7 @@ public class AppTest {
 
 	@Test
 	public void basicTest() {
-		assertTrue(true);
+		assertFalse(true);
 	}
 
 }
