bda99473975048485deb538f29e28fe1c0db3ceb
Ana Mateo <anamateo@tuenti.com>
2014-11-04 16:35:27 +0100

    added test

--- src/test/java/AppTest.java
+++ src/test/java/AppTest.java
@@ -1,9 +1,16 @@
+import org.junit.Test;
+
+import static org.junit.Assert.assertTrue;
+
 /**
  * Created by anamateo on 04/11/14.
  */
 public class AppTest {
 
 
-
+	@Test
+	public void basicTest() {
+		assertTrue(true);
+	}
 
 }
